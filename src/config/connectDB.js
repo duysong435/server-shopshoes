46120399f5098cb8ae7ef5bd351ebab360d6b789
Duy Sóng <duysong435@gmail.com>
2023-05-15 13:15:14 +0700

    deploy server

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -7,10 +7,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, pr
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
+        ssl: true
     }
 });
 
